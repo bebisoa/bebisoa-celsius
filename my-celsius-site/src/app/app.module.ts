67dd53193b91ebb08e6522e489d56d88eba316f2
bebisoa <razafyrabe1@gmail.com>
2022-05-19 22:25:19 +0200

    2 files modified

--- my-celsius-site/src/app/app.module.ts
+++ my-celsius-site/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,8 +9,14 @@ import { BienvenueComponent } from './bienvenue/bienvenue.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, BienvenueComponent, InscriptionComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BienvenueComponent,
+    InscriptionComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- my-celsius-site/src/app/inscription/inscription.component.ts
+++ my-celsius-site/src/app/inscription/inscription.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <section class="hero-is-link is bold">
     <div class="hero-body">
-      <div class="container content has text-centered">
+      <div class="container content has-text-centered">
         <h1 class="title">Inscrivez-vous ici!</h1>
        </div>
      </div>
@@ -15,35 +15,35 @@ import { Component, OnInit } from '@angular/core';
   <div class="field">
     <label class="label">Nom</label>
     <div class="control">
-      <input class="input" type="text">
+      <input type="text" name="nom" class="input" [(ngModel)]="nom">
     </div>
   </div>
 
   <div class="field">
     <label class="label">Prénom</label>
     <div class="control">
-      <input class="input" type="text">
+      <input type="text" name="prénom" class="input" [(ngModel)]="prenom">
     </div>
   </div>
 
   <div class="field">
     <label class="label">Email</label>
     <div class="control">
-      <input class="input" type="email" placeholder="ex: is@exemple.com">
+      <input type="email" name="email" class="input" [(ngModel)]="email">
     </div>
   </div>
 
 <div class="field">
   <label class="label">Mot de passe</label>
   <div class="control">
-    <input class="input" type="password" placeholder="**********">
+    <input type="password" name="mot de passe" class="input" [(ngModel)]="password">
   </div>
 </div>
 
 <div class="field">
   <label class="label">Confirmer votre mot de passe</label>
   <div class="control">
-    <input class="input" type="password" placeholder="**********">
+    <input type="password" name="vérification mot de passe" class="input" [(ngModel)]="verification">
   </div>
 </div>
 
@@ -53,7 +53,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class InscriptionComponent implements OnInit {
+  nom: string;
+  prenom: string;
+  email: string;
+  password: string;
+  verification: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  submitForm() {}
 }
